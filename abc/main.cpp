de532d1a39e7a594e3a0b95dd7924b4d1b55095e
ps16atanaka <kyelese0617@gmail.com>
2017-06-08 06:09:40 +0900

    Initial Commit

---
+++ abc/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  abc
+//
+//  Created by 田中皓己 on 2017/06/08.
+//  Copyright © 2017年 aa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
